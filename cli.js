e0a7af9a1125b20bc6dc0ccd3a3d9abc3f25f596
John Adams <github@borilla.co.uk>
2017-03-30 15:15:28 +0800

    Show help if command is run without any parameters
    Also, add appropriate process.exitCode on success/error

--- cli.js
+++ cli.js
@@ -4,8 +4,11 @@ var program = require('commander');
 var package = require('./package.json');
 var makeCss = require('./index');
 var fs = require('fs');
-var complete = false;
-var options;
+var isComplete = false;
+var options, exitCode;
+
+// *hack* to set command name to show in help from name defined in package.json
+process.argv[1] = Object.keys(package.bin)[0];
 
 program
 	.version(package.version)
@@ -26,15 +29,22 @@ options = {
 	height: program.height
 };
 
-makeCss(options).then(function (result) {
-	var filePath = program.args[0];
-	fs.writeFile(filePath, result.css);
-	console.log('written directives for ' + result.info.length + ' images to ' + filePath);
-	complete = true;
-}).catch(function (error) {
-	console.error(error);
-	complete = true;
-});
+// if no args provided then show help
+if (!process.argv.slice(2).length) {
+	program.outputHelp();
+	exitCode = 0;
+}
+else {
+	makeCss(options).then(function (result) {
+		var filePath = program.args[0];
+		fs.writeFile(filePath, result.css);
+		console.log('written directives for ' + result.info.length + ' images to ' + filePath);
+		exitCode = 0;
+	}).catch(function (error) {
+		console.error(error);
+		exitCode = 1;
+	});
+}
 
 function parsePath(val) {
 	var lastChar =- val[val.length - 1];
@@ -44,8 +54,12 @@ function parsePath(val) {
 	return val;
 }
 
+// prevent script from exiting before completion of promises
 (function waitForCompletion () {
-	if (!complete) {
-		setTimeout(waitForCompletion, 200);
+	if (exitCode === undefined) {
+		setTimeout(waitForCompletion, 250);
+	}
+	else {
+		process.exitCode = exitCode;
 	}
 })();
